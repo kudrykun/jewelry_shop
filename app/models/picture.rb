2fac69f2b903f0d24f77af6c67e8b5380041831a
Sergey Vasilenko <kudrykun@gmail.com>
2017-07-05 17:12:51 +0300

    Добавлен Paperclip к моделям

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,4 +4,8 @@ class Picture < ApplicationRecord
   Применяется как для использования в других сущностях(например в товарах), так и, например, в слайдерах.
 =end
   belongs_to :imageable, polymorphic: true
+
+
+  has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
