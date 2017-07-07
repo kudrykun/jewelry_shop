ffcee6bbc46517673fd574a2531b5ac5add77f8a
Sergey Vasilenko <kudrykun@gmail.com>
2017-07-07 23:38:18 +0300

    Связал наборы и товары, почистил новый контроллер

--- app/controllers/admin/kits_controller.rb
+++ app/controllers/admin/kits_controller.rb
@@ -1,53 +1,36 @@
 class KitsController < ApplicationController
   before_action :set_kit, only: [:show, :edit, :update, :destroy]
-
-  # GET /kits
-  # GET /kits.json
+  
   def index
     @kits = Kit.all
   end
-
-  # GET /kits/1
-  # GET /kits/1.json
+  
   def show
   end
-
-  # GET /kits/new
+  
   def new
     @kit = Kit.new
   end
-
-  # GET /kits/1/edit
+  
   def edit
   end
-
-  # POST /kits
-  # POST /kits.json
+  
   def create
     @kit = Kit.new(kit_params)
-
-    respond_to do |format|
-      if @kit.save
-        format.html { redirect_to @kit, notice: 'Kit was successfully created.' }
-        format.json { render :show, status: :created, location: @kit }
-      else
-        format.html { render :new }
-        format.json { render json: @kit.errors, status: :unprocessable_entity }
-      end
+    if @kit.save
+      redirect_to @kit, notice: 'Kit was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /kits/1
   # PATCH/PUT /kits/1.json
   def update
-    respond_to do |format|
-      if @kit.update(kit_params)
-        format.html { redirect_to @kit, notice: 'Kit was successfully updated.' }
-        format.json { render :show, status: :ok, location: @kit }
-      else
-        format.html { render :edit }
-        format.json { render json: @kit.errors, status: :unprocessable_entity }
-      end
+    if @kit.update(kit_params)
+      redirect_to @kit, notice: 'Kit was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -55,10 +38,7 @@ class KitsController < ApplicationController
   # DELETE /kits/1.json
   def destroy
     @kit.destroy
-    respond_to do |format|
-      format.html { redirect_to kits_url, notice: 'Kit was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to kits_url, notice: 'Kit was successfully destroyed.'
   end
 
   private
