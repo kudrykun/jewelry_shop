1747d141109ef9a1c039aa1055da006718636a03
Sergey Vasilenko <kudrykun@gmail.com>
2017-07-07 21:31:52 +0300

    починил виды материалов(metal types)

--- app/controllers/admin/metal_types_controller.rb
+++ app/controllers/admin/metal_types_controller.rb
@@ -28,7 +28,7 @@ class Admin::MetalTypesController < Admin::AdminController
 
     respond_to do |format|
       if @metal_type.save
-        format.html { redirect_to @metal_type, notice: 'Metal type was successfully created.' }
+        format.html { redirect_to admin_metal_type_path(@metal_type), notice: 'Metal type was successfully created.' }
         format.json { render :show, status: :created, location: @metal_type }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class Admin::MetalTypesController < Admin::AdminController
   def update
     respond_to do |format|
       if @metal_type.update(metal_type_params)
-        format.html { redirect_to @metal_type, notice: 'Metal type was successfully updated.' }
+        format.html { redirect_to admin_metal_type_path(@metal_type), notice: 'Metal type was successfully updated.' }
         format.json { render :show, status: :ok, location: @metal_type }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class Admin::MetalTypesController < Admin::AdminController
   def destroy
     @metal_type.destroy
     respond_to do |format|
-      format.html { redirect_to metal_types_url, notice: 'Metal type was successfully destroyed.' }
+      format.html { redirect_to admin_metal_types_url, notice: 'Metal type was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
