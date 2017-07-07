9faba1262c8dacf6152765704d5d98943e567278
Sergey Vasilenko <kudrykun@gmail.com>
2017-07-07 15:11:14 +0300

    Создал категории и промежуточную таблицу с типами товаров

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "admin/products#index"
   namespace :admin do
     get '/', to: 'dashboard#index', as: 'dashboard'
+    resources :categories
     resources :collections
     resources :products
     resources :sale_sizes
--- test/models/metal_type_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MetalTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
