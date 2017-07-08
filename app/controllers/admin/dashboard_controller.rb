b23e9b22add4e5c9c81f5997c0e2fc8828c77ac1
iliyaking46 <iliyaking46@gmail.com>
2017-07-08 16:41:02 +0300

    Обновил datatables responsive, продукт создается без категории, изменил dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < Admin::AdminController
   def index
+    @products = Product.all
+    @categories = Category.all
+    @collections = Collection.all
+    @kits = Kit.all
   end
 end
