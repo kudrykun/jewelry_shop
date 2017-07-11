df09f17c6fa0f8533147a16582b0884e4b50c4bd
Sergey Vasilenko <kudrykun@gmail.com>
2017-07-12 00:29:25 +0300

    Приготовил seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,12 @@
 Faker::Config.locale = 'ru'
 
 
-Product.delete_all
+Product.all.each do |product|
+  product.incrustations = []
+  product.sizes = []
+  product.metal_types = []
+  product.delete
+end
 Category.delete_all
 Collection.delete_all
 Kit.delete_all
@@ -25,25 +30,24 @@ MetalType.delete_all
 category_titles = ["Кольца","Подвески","Цепочки","Браслеты","Колье","Ожерелья","Бусы","Кулоны","Пирсы","Запонки","Иконы","Часы","Столовые приборы","Сувениры"]
 collection_titles = ["Геометрия","Золото и бриллианты","Вальс тюльпанов","Цветочный рай","Природные мотивы","Золото и бриллианты","Цветной водопад","Солнечное царство","Antique","Эволюция"]
 incrustation_titles = ["Алмаз"," Рубин"," Сапфир"," Аквамарин"," Изумруд"," Александрит"," Гранаты"," Аметист"," Опал благородный"," Опал огненный"," Топаз"," Жемчуг"," Янтарь"," Коралл"]
+kit_titles = ["Альфа","Бета","Гамма","Дельта","Эпсилон","Дзета","Эта","Тета","Йота","Каппа","Лямбда","Омикрон"]
+size_titles = ["15","15.5","16","16.5","17","17.5","18","18.5","19","19.5","20"]
+sale_Size_percent = [10,20,30]
+metal_color_titles = ["Белый","Желтый","Красный","Розовый"]
+metal_type_titles = ["Золото(585)","Серебро(825)","Красное золото","Белое золото"]
+product_type_titles = ["Полудраг","С драгоценностями"]
 
-
-products_size = 10
+products_size = 1000
 categories_size = category_titles.size
 collections_size = collection_titles.size
 incrustations_size = incrustation_titles.size
-kits_size = 10
-incrustations_size = 10
-productTypes_size = 10
-saleSizes_size = 10
-sizes_size = 10
-incrustations_size = 10
-metalColors_size = 10
-metalTypes_size = 10
-incrustations = Incrustation.create([{title: 'Алмаз'},
-                                     {title: 'Фионит'},
-                                     {title: 'Рубин'},
-                                     {title: 'Изумруд'},
-                                     {title: 'Янтарь'}])
+kits_size = kit_titles.size
+productTypes_size = product_type_titles.size
+saleSizes_size = sale_Size_percent.size
+sizes_size = size_titles.size
+metalColors_size = metal_color_titles.size
+metalTypes_size = metal_type_titles.size
+
 
 metal_colors = MetalColor.create([{title: 'Жёлтый'},
                                      {title: 'Белый'},
@@ -55,13 +59,6 @@ metal_types = MetalType.create([{title: 'Золото (585)'},
 product_types = ProductType.create([{title: 'С драгоценностями'},
                                    {title: 'Полудраг'}])
 
-sale_sizes = SaleSize.create([{sale_percent: 10},
-                                    {sale_percent: 20},
-                                    {sale_percent: 50}])
-
-sizes = Size.create([{size: '16'},
-                     {size: '16.5'},
-                     {size: '17'}])
 categories = []
 categories_size.times do
   categories << Category.create(title: category_titles.delete(category_titles.sample),
@@ -79,19 +76,57 @@ incrustations = []
 incrustations_size.times do
   incrustations << Incrustation.create(title: incrustation_titles.delete(incrustation_titles.sample))
 end
-=begin
-products = Product.create([{title: 'Кольцо',
-                            artikul: '12345',
-                            price: 1000.23,
-                            metal_color: metal_colors[1],
-                           weight: 13.5,
-                           product_type: product_types[0],
-                           sale_size: sale_sizes[1],
-                           old_price: 2000,
-                           to_main_page: true,
-                           manufacturer: 'Sokoloff',
-                           priority: 3,
-                           incrustations: [incrustations[2],incrustations[3]],
-                           metal_types: [metal_types[0]],
-                           size: [sizes[0],sizes[1],sizes[2]]}])
-=end
+
+sizes = []
+sizes_size.times do
+  sizes << Size.create(size: size_titles.delete(size_titles.sample))
+end
+
+saleSizes = []
+saleSizes_size.times do
+  saleSizes << SaleSize.create(sale_percent: sale_Size_percent.delete(sale_Size_percent.sample))
+end
+
+metalColors = []
+metalColors_size.times do
+  metalColors << MetalColor.create(title: metal_color_titles.delete(metal_color_titles.sample))
+end
+
+metalType = []
+metalTypes_size.times do
+  metalType << MetalType.create(title: metal_type_titles.delete(metal_type_titles.sample))
+end
+
+kits = []
+kits_size.times do
+  kits << Kit.create(title: kit_titles.delete(kit_titles.sample))
+end
+
+product_types = []
+productTypes_size.times do
+  product_types << ProductType.create(title: product_type_titles.delete(product_type_titles.sample))
+end
+
+products = []
+products_size.times do |i|
+  products << Product.create(title: "Товар #{i}",
+                             price: Faker::Number.between(5000,50000),
+                             metal_color: metal_colors.sample,
+                              artikul: Faker::Code.asin,
+                              weight: Faker::Number.between(5,30),
+                              product_type: product_types.sample,
+                              sale_size: Faker::Boolean.boolean(0.2) ? saleSizes.sample : nil,
+                              new_price: Faker::Number.between(5000,50000),
+                              to_main_page: Faker::Boolean.boolean(0.1),
+                              manufacturer: Faker::Company.name,
+                              priority: Faker::Number.between(1,products_size),
+                              sex: Faker::Number.between(0,3),
+                              category: categories.sample,
+                              collection: Faker::Boolean.boolean(0.4) ? collections.sample : nil,
+                              kit: Faker::Boolean.boolean(0.2) ? kits.sample : nil)
+  products.last.incrustations << incrustations.sample(Faker::Number.between(0,3))
+  products.last.metal_types << metal_types.sample(Faker::Number.between(0,2))
+  products.last.sizes << sizes.sample(Faker::Number.between(1,5))
+
+end
+
