7db6ccc0b66a291b52986b81d5d96a70fb8b7560
iliyaking46 <iliyaking46@gmail.com>
2017-08-17 13:28:24 +0300

    Изменил вид добавления вставок и еще по мелочи

--- app/assets/javascripts/admin/nested_forms.js
+++ app/assets/javascripts/admin/nested_forms.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     /*при нажатии на чекбокс или его подобие*/
     $(document).on('change', '.destroy-incrustation', function () {
-        $(this).parent().fadeOut(); /*Предполагется, что исчезает весь контейнер со связанными полями*/
-        if ($(this).parent().hasClass('dynamically-added')) {
+        $(this).parent().parent().fadeOut(); /*Предполагется, что исчезает весь контейнер со связанными полями*/
+        if ($(this).parent().parent().hasClass('dynamically-added')) {
             /*Если этот контейнер был добавлен динамически, то подчищаем html*/
-            $(this).parent().remove();
+            $(this).parent().parent().remove();
         }else{
             /*Если он добавлен не динамически, то оставляем его, чтобы данные на удаление были переданы в контроллер*/
             $(this).prop('checked',true);
