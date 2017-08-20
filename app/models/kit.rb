c1f38d6000c924ed327110130f828e607872c701
iliyaking46 <iliyaking46@gmail.com>
2017-08-20 16:56:03 +0300

    Добавил цену за комплект

--- app/models/kit.rb
+++ app/models/kit.rb
@@ -2,4 +2,31 @@ class Kit < ApplicationRecord
   has_many :products
 
   validates :title, presence: true, length: {in: 1..50}
+
+  def getKitPrice
+    kit_price = 0
+    self.products.each do |product|
+      if product.price != nil
+        kit_price += product.price
+      end
+    end
+    #если в конце метода указываешь значене какое то, то это типа return
+    kit_price
+  end
+
+  def getKitNewPrice
+    kit_price = 0
+    self.products.each do |product|
+      if product.new_price != nil
+        kit_price += product.new_price
+      else
+        if product.price != nil
+          kit_price += product.price
+        end
+      end
+    end
+    #если в конце метода указываешь значене какое то, то это типа return
+    kit_price
+  end
+
 end
