6de4c2125c49c6f25cef9666ddf6c130d00a4301
Sergey Vasilenko <kudrykun@gmail.com>
2017-08-26 01:23:02 +0300

    Добавил описание в производителя

---
+++ db/migrate/20170825221654_add_description_to_manufacturer.rb
@@ -0,0 +1,5 @@
+class AddDescriptionToManufacturer < ActiveRecord::Migration[5.0]
+  def change
+    add_column :manufacturers, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824214406) do
+ActiveRecord::Schema.define(version: 20170825221654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,8 +61,9 @@ ActiveRecord::Schema.define(version: 20170824214406) do
 
   create_table "manufacturers", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "metal_colors", force: :cascade do |t|
@@ -163,9 +164,9 @@ ActiveRecord::Schema.define(version: 20170824214406) do
   end
 
   create_table "sizes", force: :cascade do |t|
-    t.string   "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "size"
   end
 
   add_foreign_key "incrustation_items", "incrustations"
