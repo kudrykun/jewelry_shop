b3d84db6ee520cd94b9cc039a7ec39ab79a68e52
iliyaking46 <iliyaking46@gmail.com>
2017-08-26 16:29:40 +0300

    Перенес рендеры, добавил отдельно шаблон для главной

--- app/controllers/store/main_page_controller.rb
+++ app/controllers/store/main_page_controller.rb
@@ -1,4 +1,5 @@
 class Store::MainPageController < Store::StoreController
+  layout 'main_page'
   def index
     @products_hit = Product.where(visible: true).where(hit: true).order(priority: :desc).order(updated_at: :desc)
     @products15 = Product.where(visible: true).where(to_main_page: true).order(priority: :desc).order(updated_at: :desc)
