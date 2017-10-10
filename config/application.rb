e4a841ea8fd86df0b4b5e567e88a466a2f7c5345
iliyaking46 <iliyaking46@gmail.com>
2017-10-10 20:07:56 +0300

    Добавил другие шрифты, несколько иконок

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module JewelryShop
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # config.assets.paths << Rails.root.join("app", "assets", "fonts") Может пригодиться
     config.i18n.default_locale = :ru
   end
 end
