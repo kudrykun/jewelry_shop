009eb2c07873ff92b0cb6158de1446096c9217fb
kudrykun <kudrykun@gmail.com>
2017-10-15 16:39:31 +0300

    Ошибка со скидкой 40% 50%

--- app/controllers/store/main_page_controller.rb
+++ app/controllers/store/main_page_controller.rb
@@ -11,9 +11,21 @@ class Store::MainPageController < Store::StoreController
     @products_hit = Product.where(visible: true).where(hit: true).where.not(preview: nil).order(priority: :desc).order(updated_at: :desc)
 
     # Продукты на главную страницу со скидкой 40%
-    @products40 = SaleSize.all.find_by(sale_percent: 40).products.where(visible: true).where(to_main_page: true).where.not(preview: nil).order(priority: :desc).order(updated_at: :desc)
+    sale40 = SaleSize.find_by(sale_percent: 40)
+    @products40 = []
+    if !sale40.nil?
+      @products40 = sale40.products.where(visible: true).where(to_main_page: true).where.not(preview: nil).order(priority: :desc).order(updated_at: :desc)
+    else
+      @products40
+    end
 
     # Продукты на главную страницу со скидкой 50%
-    @products50 = SaleSize.all.find_by(sale_percent: 50).products.where(visible: true).where(to_main_page: true).where.not(preview: nil).order(priority: :desc).order(updated_at: :desc)
+    sale50 = SaleSize.all.find_by(sale_percent: 50)
+    @products50 = []
+    if !sale50.nil?
+      @products50 = sale50.products.where(visible: true).where(to_main_page: true).where.not(preview: nil).order(priority: :desc).order(updated_at: :desc)
+    else
+      @products50
+    end
   end
 end
