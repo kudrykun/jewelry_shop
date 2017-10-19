fae6ae6d11f74c2100437acabe3025a5a22f5334
kudrykun <kudrykun@gmail.com>
2017-10-19 21:38:10 +0300

    Изменил роуты входа-выхода

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, :skip => [:registrations]
+  devise_for :users, :skip => [:registrations,:sessions]
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
   root 'store/main_page#index'
 
   get 'products/:id', to: 'store/products#show', as: 'product'
