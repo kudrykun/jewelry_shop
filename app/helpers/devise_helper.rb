23da48b3921436209086ac3524f5de79023651c6
iliyaking46 <iliyaking46@gmail.com>
2017-10-20 01:55:56 +0300

    Поменял вьюхи девайса

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,22 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" unless devise_error_messages?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+    html = <<-HTML
+    <div class="alert alert-danger alert-password" role="alert" id="error_explanation">
+      <ul class="list-unstyled">
+        #{messages}
+      </ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+
+end
