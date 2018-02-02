993fe8e43ce8a7e9b3667c648d751f8f1ae5c54c
kudrykun <kudrykun@gmail.com>
2018-02-02 16:13:02 +0300

    Таки новая версия руби

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :linked_dirs, %w(log tmp/cache tmp/pids vendor/bundle public/system)
 # set :keep_releases, 5
 
 # Configure RVM
-set :rvm_ruby_version, '2.2'
+set :rvm_ruby_version, '2.4'
 
 # You unlikely have to change below this line
 # -----------------------------------------------------------------------------
